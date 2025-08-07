8f76978b3f09781ad3eaa445f805c9ac9168cf48
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-08-07 22:28:40 +0530

    feat: create database schema for game manager
    - add populatedb sccript to create schema

--- db/populatedb.js
+++ db/populatedb.js
@@ -13,7 +13,49 @@ const dbName = process.env.DB_NAME;
 const connectionString =
 	argv[2] || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`;
 
-const psqlText = ``;
+const psqlText = `
+	CREATE TABLE IF NOT EXISTS games (
+		id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+		title VARCHAR ( 255 ) UNIQUE NOT NULL,
+		release_date TIMESTAMPTZ DEFAULT NOW(),
+		description TEXT,
+		rating DECIMAL(2, 1) CHECK (rating >=0 AND rating <=5),
+		price DECIMAL (10, 2),
+		created_at TIMESTAMPTZ DEFAULT NOW(),
+		updated_at TIMESTAMPTZ DEFAULT NOW(),
+		developer_id INTEGER NOT NULL REFERENCES developers (id)
+	);
+
+	CREATE TABLE IF NOT EXISTS developers (
+		id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+		name VARCHAR ( 255 ) UNIQUE NOT NULL,
+		description TEXT NOT NULL,
+		followers INTEGER DEFAULT 0 CHECK (followers >= 0),
+		founding_date DATE DEFAULT CURRENT_DATE() CHECK (founding_date <= CURRENT_DATE)
+	);
+
+	CREATE TABLE IF NOT EXISTS genres (
+		id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+		type VARCHAR ( 255 ) NOT NULL UNIQUE
+	);
+		
+	CREATE TABLE IF NOT EXISTS categories (
+		id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+		type VARCHAR ( 255 ) NOT NULL UNIQUE
+	);
+
+	CREATE TABLE IF NOT EXISTS games_genres (
+		game_id INTEGER NOT NULL REFERENCES games (id),
+		genres_id INTEGER NOT NULL REFERENCES genres (id),
+		PRIMARY KEY (game_id, genres_id)
+	);
+		
+	CREATE TABLE IF NOT EXISTS games_categories (
+		game_id INTEGER NOT NULL REFERENCES games (id),
+		categories_id INTEGER NOT NULL REFERENCES categories (id),
+		PRIMARY KEY (game_id, categories_id)
+	);
+`;
 const params = [];
 
 async function main() {
